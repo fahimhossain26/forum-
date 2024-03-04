603396cfb14d0339694002cac81e0be8a166fe42
fahimhossain26 <ismailhossainfahim64@gmail.com>
2024-03-04 20:16:42 +0600

    leftside container

--- script.js
+++ script.js
@@ -105,20 +105,21 @@ const leftContainer=(title,view)=>{
     const div=document.createElement('div');
     div.innerHTML=`
     <div class="flex bg-white p-4 rounded-2xl w-72 mb-2 ">
-        <h2>${title}</h2>
+        <h2> ${title? title:''}</h2>
        <div class="flex gap-1">
         
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
+        <svg  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+        
+        <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
             <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
           </svg>
           
-        <p>${view}</p>
+        <p>${view? view:''}</p>
        </div>
    
     </div>
     `
-    
+   
     text.appendChild(div);
     
 };
@@ -156,7 +157,7 @@ const data =await res.json();
         <div >
             <h2 class="card-title">${item.author.name}</h2>
             
-            <p class="mt-1">${item.author.designation}</p>
+            <p class="mt-1">${item.author.designation? item.author.designation :'Unknown'}</p>
         </div>
     </div>
     </div>
